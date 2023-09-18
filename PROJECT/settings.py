1c36ec21a67d06e84765ab742b84857d8bee65a0
ulugbek101 <viper.ghost@bk.ru>
2023-09-18 17:11:05 +0500

    Add facebook authoraization and authentication but it is not working for some reasons

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 ]
 
 MIDDLEWARE = [
@@ -144,6 +145,28 @@ SOCIALACCOUNT_PROVIDERS = {
             'email',
         ],
         'AUTH_PARAMS': {'access_type': 'online'}
+    },
+    'facebook': {
+        'METHOD': 'oauth2',  # Set to 'js_sdk' to use the Facebook connect SDK
+        # 'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
+        'SCOPE': ['email', 'public_profile'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'name',
+            'name_format',
+            'picture',
+            'short_name'
+        ],
+        'EXCHANGE_TOKEN': True,
+        # 'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v13.0',
+        'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
     }
 }
 AUTHENTICATION_BACKENDS = (
--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ def index(request):
     if not request.user.is_authenticated:
         return redirect('account_login')
     else:
-        return HttpResponse(f'You are logged in as {request.user.email}')
+        return HttpResponse(f'You are logged in as {request.user.email}, to logout press <a href="logout">Logout</a>')
 
 def logout_view(request):
     logout(request)
