287fd5b619088241ed155ed2e7ca41e0375e1d00
ulugbek101 <thedevu101@gmail.com>
2023-09-20 09:37:13 +0500

    Separate signin and signup html components and configure their input fields' attributes

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
     path('logout/', views.logout_view),
+
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='account_login')
 def index(request):
     if not request.user.is_authenticated:
         return redirect('account_login')
@@ -11,3 +13,14 @@ def index(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+
+def signin(request):
+    # email = request.POST.get('email')
+    # password = request.POST.get('password')
+    # next = request.POST.get('next')
+    pass
+
+
+def signup(request):
+    pass
