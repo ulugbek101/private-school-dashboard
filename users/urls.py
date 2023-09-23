d9b03086be3d1fc2159878c67ce1f8e1dec366af
ulugbek101 <thedevu101@gmail.com>
2023-09-23 18:27:44 +0500

    Configure authentication with email and password

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('logout/', LogoutView.as_view()),
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @login_required(login_url='account_login')
@@ -17,10 +20,27 @@ def logout_view(request):
 
 
 def signin(request):
-    # email = request.POST.get('email')
-    # password = request.POST.get('password')
-    # next = request.POST.get('next')
-    pass
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    next_page = request.POST.get('next')
+
+    try:
+        user = User.objects.get(email=email)
+    except:
+        user = None
+
+    if user:
+        password_is_correct = user.check_password(password)
+        if password_is_correct:
+            # success message
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('home')
+        else:
+            # password incorrect error message
+            return redirect('account_login')
+    else:
+        # user not found error message
+        return redirect('account_login')
 
 
 def signup(request):
