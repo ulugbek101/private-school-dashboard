fbd2566ee138eafdddd9df00757cf2b1e9914130
ulugbek101 <viper.ghost@bk.ru>
2023-09-24 22:49:57 +0500

    Configure static and media files storage and paths

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -17,8 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(prefix=settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
